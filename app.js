2b2bfb6f55225fc870b43ba15764b86a63d35f3a
Leo Wang <leowang801@gmail.com>
2023-04-01 13:00:09 -0700

    link backend to frontend

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // app.js is the entry point of the application
 const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
 
 const app = express();
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
