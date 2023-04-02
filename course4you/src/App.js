d0c1b3d54d675c79201a7e12dd41e1b3f5dc4ffc
Leo Wang <leowang801@gmail.com>
2023-04-01 21:53:33 -0700

    finish welcome page

--- course4you/src/App.js
+++ course4you/src/App.js
@@ -18,12 +18,39 @@ export default function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={
-            <div className="App-header">
-              Welcome to Course4You
-              <button className="login-button" onClick={navigateToLogin}>
-                Login
-              </button>
-          </div>} />
+          <div className="home-background">
+            <div className="home-gradient"></div>
+
+            <h1 className="welcome-message"> 
+              Welcome to Course4You! 
+            </h1>
+
+            <h2 className="welcome-sub-message"> 
+              For students, by students! 
+            </h2>
+
+            <p className="welcome-text"> 
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Ut et massa mi. Aliquam in hendrerit urna. Pellentesque 
+              sit amet sapien fringilla, mattis ligula consectetur, 
+              ultrices mauris. Maecenas vitae mattis tellus. Nullam 
+              quis imperdiet augue. Vestibulum auctor ornare leo, non 
+              suscipit magna interdum eu. Curabitur pellentesque nibh 
+              nibh, at maximus ante fermentum sit amet.
+            </p>
+
+            <div className='welcome-image'>
+              
+            </div>
+
+            <button 
+              className="register-button" 
+              onClick={navigateToLogin}>
+              Register
+            </button>
+          </div>
+        } />
+
         <Route path="/pages/login" element={<Login />} />
         <Route path="/pages/courseFinder/courseFinder" element={<CourseFinder/>} />
       </Routes>
