f7dad6135adb2c9868c0f40737b79ebece458429
Leo Wang <leowang801@gmail.com>
2023-04-01 18:36:09 -0700

    style registeration page

--- course4you/src/pages/login.js
+++ course4you/src/pages/login.js
@@ -1,12 +1,109 @@
 import React from 'react';
+import './login.css';
 
 export default function Login() {
     console.log('Login page rendered');
 
-    
+
     return (
-        <div className="login">
-        <h1>Login</h1>
+        <>
+        <div className="register">
+            <h1 className='create-account-text'>
+                Create an account today
+            </h1>
+
+            <div className = 'email'>
+                <label
+                  className="email-text"
+                >
+                  Email
+                </label>
+                <div>
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    //value={email}
+                    //onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="email-input"
+                    />
+                </div>
+            </div>
+
+            <div className = 'password'>
+                <label
+                  className="password-text"
+                >
+                  Password
+                </label>
+                <div>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="password"
+                    //value={[password]}
+                    //onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="password-input"
+                    />
+                </div>
+            </div>
+
+            <div className = 'student-number'>
+                <label
+                  className="student-number-text"
+                >
+                  StudentID
+                </label>
+                <div>
+                  <input
+                    id="student-number"
+                    name="student-number"
+                    type="text"
+                    autoComplete="student-number"
+                    //value={[password]}
+                    //onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="student-number-input"
+                    />
+                </div>
+            </div>
+
+            <div className = 'major'>
+                <label
+                  className="major-text"
+                >
+                  Major
+                </label>
+                <div>
+                  <input
+                    id="major"
+                    name="major"
+                    type="major"
+                    autoComplete="major"
+                    //value={[password]}
+                    //onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="major-input"
+                    />
+                </div>
+            </div>
+
+            <div className = 'continue'>
+                <button 
+                type='submit'
+                //onClick={onSubmit}     
+                className="continue-button">
+                    Continue
+                </button>
+            </div>
+
+
         </div>
+        </>
+
     );
 }
